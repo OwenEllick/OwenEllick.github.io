34453b12b65a181043ca59b6d8b4ffead36fa0da
OwenEllick <ome1@geneseo.edu>
2025-01-27 16:18:19 -0500

    R basics

---
+++ Dropdownmenu.R
@@ -0,0 +1,18 @@
+#drop down menu
+left:
+  - text: "Projects"
+    menu:
+      - project_1.qmd
+      - project_2.qmd 
+    
+install.packages("knitr")
+install.packages("rmarkdown")
+install.packages("gapminder")
+install.packages("tidyverse")
+install.packages("ggplot")
+install.packages("skimr")
+install.packages("ggthemes")
+install.packages("hrbrthemes")
+install.packages("DT")
+
+
